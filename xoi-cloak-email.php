bc60b5540309137a361048b30bf9c9fb1630abed
Izzy Lively <4278716+izzylively@users.noreply.github.com>
2022-04-25 23:15:28 -0500

    Create xoi-cloak-email.php

---
+++ xoi-cloak-email.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Plugin Name: XOI Cloak Email
+ * Plugin Description: Simple email cloaking shortcode for WP.
+ * Plugin URI: https://github.com/izzylively/xoi-cloak-email/releases/
+ * Author: Izzy Lively
+ * Author URI: https://github.com/izzylively/xoi-cloak-email/releases/
+ * Text Domain: xoi-cloak-email
+ * License: GPL 3+
+ * Version: 1.0.0
+*/
+
+// Add Shortcode
+function email_cloaking_shortcode( $atts ) {
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'email' => '',
+		),
+		$atts,
+		'cloak'
+	);
+	// Return cloaked email
+	return antispambot( $atts['email'] );
+}
+add_shortcode( 'cloak', 'email_cloaking_shortcode' );
+
+?>
